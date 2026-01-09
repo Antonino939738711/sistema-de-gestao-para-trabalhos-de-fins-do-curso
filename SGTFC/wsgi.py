4f43936e2ca9df830b4f5b6343916b4470274262
Antonino939738711 <antoninocaladojacobteodoro@gmail.com>
2026-01-09 21:51:47 +0000

    final rodando na vercel

--- SGTFC/wsgi.py
+++ SGTFC/wsgi.py
@@ -3,4 +3,4 @@ from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'SGTFC.settings')
 
-app = get_wsgi_application()  # Vercel usa este nome
+app = get_wsgi_application()
